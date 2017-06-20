67789fd7cf66ab7a7e0a1c9c9c11db798a019644
Rajat Dhamija <droidoselabs@gail.com>
2017-06-21 02:24:54 +0530

    Image Picker Added

--- app/src/main/java/in/droidoselabs/myapplication/activity/AddFeedActivity.java
+++ app/src/main/java/in/droidoselabs/myapplication/activity/AddFeedActivity.java
@@ -1,15 +1,82 @@
 package in.droidoselabs.myapplication.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.gun0912.tedpicker.Config;
+import com.gun0912.tedpicker.ImagePickerActivity;
+
+import java.util.ArrayList;
 
 import in.droidoselabs.myapplication.R;
 
 public class AddFeedActivity extends AppCompatActivity {
 
+    private static final int INTENT_REQUEST_GET_IMAGES = 13;
+
+    private static final String TAG = "TedPicker";
+    ArrayList<Uri> image_uris = new ArrayList<Uri>();
+    LinearLayout getImages;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_feed);
+        getImages = (LinearLayout) findViewById(R.id.getImages);
+
+        getImages.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                Config config = new Config();
+                config.setSelectionMin(1);
+                config.setSelectionLimit(5);
+                config.setFlashOn(true);
+                getImages(config);
+            }
+        });
+
+
+    }
+
+    private void getImages(Config config) {
+
+
+        ImagePickerActivity.setConfig(config);
+
+        Intent intent = new Intent(this, ImagePickerActivity.class);
+
+        if (image_uris != null) {
+            intent.putParcelableArrayListExtra(ImagePickerActivity.EXTRA_IMAGE_URIS, image_uris);
+        }
+        startActivityForResult(intent, INTENT_REQUEST_GET_IMAGES);
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == INTENT_REQUEST_GET_IMAGES) {
+
+                image_uris = intent.getParcelableArrayListExtra(ImagePickerActivity.EXTRA_IMAGE_URIS);
+
+                if (image_uris != null) {
+                    Log.d(TAG, "onActivityResult: " + image_uris);
+                }
+
+
+            }
+        }
     }
 }
