e43f90c57f34211fa9b6178057227ac8bda6f9e5
cubercuber <ranveer.singh@theroboticshomepage.com>
2017-06-21 03:02:17 +0530

    AddFeedActivity ImageUris handled

--- app/src/main/java/in/droidoselabs/myapplication/activity/AddFeedActivity.java
+++ app/src/main/java/in/droidoselabs/myapplication/activity/AddFeedActivity.java
@@ -7,10 +7,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.gun0912.tedpicker.Config;
 import com.gun0912.tedpicker.ImagePickerActivity;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -22,16 +24,21 @@ public class AddFeedActivity extends AppCompatActivity {
 
     private static final String TAG = "TedPicker";
     ArrayList<Uri> image_uris = new ArrayList<Uri>();
-    LinearLayout getImages;
+    private LinearLayout getImages, parentLayout, childLayoutOne, childLayoutTwo;
+    private ImageView ivOne, ivTwo, ivThree, ivFour, ivFive;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_feed);
+        init();
+
+
+    }
+
+    private void init() {
         getImages = (LinearLayout) findViewById(R.id.getImages);
-
         getImages.setOnClickListener(new View.OnClickListener() {
-
 
             @Override
             public void onClick(View v) {
@@ -42,6 +49,15 @@ public class AddFeedActivity extends AppCompatActivity {
                 getImages(config);
             }
         });
+        parentLayout = (LinearLayout) findViewById(R.id.parent_layout);
+        childLayoutOne = (LinearLayout) findViewById(R.id.layout_two);
+        childLayoutTwo = (LinearLayout) findViewById(R.id.layout_three);
+        //image Views
+        ivTwo = (ImageView) findViewById(R.id.image_two);
+        ivThree = (ImageView) findViewById(R.id.image_three);
+        ivFour = (ImageView) findViewById(R.id.image_four);
+        ivFive = (ImageView) findViewById(R.id.image_five);
+        ivOne = (ImageView) findViewById(R.id.image_one);
 
 
     }
@@ -73,10 +89,66 @@ public class AddFeedActivity extends AppCompatActivity {
 
                 if (image_uris != null) {
                     Log.d(TAG, "onActivityResult: " + image_uris);
+                    setImages(image_uris.size());
                 }
 
 
             }
         }
     }
+
+    private void setImages(int size) {
+        if (size>0) {
+            parentLayout.setVisibility(View.VISIBLE);
+                if (size ==1) {
+                    ivOne.setVisibility(View.VISIBLE);
+                    ivOne.setImageURI(image_uris.get(0));
+                }
+                else if (size == 2) {
+                    childLayoutOne.setVisibility(View.VISIBLE);
+                    ivOne.setVisibility(View.VISIBLE);
+                    ivTwo.setVisibility(View.VISIBLE);
+                    ivOne.setImageURI(image_uris.get(0));
+                    ivTwo.setImageURI(image_uris.get(1));
+                }
+                else if (size == 3) {
+                    childLayoutOne.setVisibility(View.VISIBLE);
+                    ivOne.setVisibility(View.VISIBLE);
+                    ivTwo.setVisibility(View.VISIBLE);
+                    ivThree.setVisibility(View.VISIBLE);
+                    ivOne.setImageURI(image_uris.get(0));
+                    ivTwo.setImageURI(image_uris.get(1));
+                    ivThree.setImageURI(image_uris.get(2));
+                }
+                else if (size == 4) {
+                    childLayoutOne.setVisibility(View.VISIBLE);
+                    childLayoutTwo.setVisibility(View.VISIBLE);
+                    ivOne.setVisibility(View.VISIBLE);
+                    ivTwo.setVisibility(View.VISIBLE);
+                    ivThree.setVisibility(View.VISIBLE);
+                    ivFour.setVisibility(View.VISIBLE);
+                    ivOne.setImageURI(image_uris.get(0));
+                    ivTwo.setImageURI(image_uris.get(1));
+                    ivThree.setImageURI(image_uris.get(2));
+                    ivFour.setImageURI(image_uris.get(3));
+                }
+                else if (size == 5) {
+                    childLayoutOne.setVisibility(View.VISIBLE);
+                    childLayoutTwo.setVisibility(View.VISIBLE);
+                    ivOne.setVisibility(View.VISIBLE);
+                    ivTwo.setVisibility(View.VISIBLE);
+                    ivThree.setVisibility(View.VISIBLE);
+                    ivFour.setVisibility(View.VISIBLE);
+                    ivFive.setVisibility(View.VISIBLE);
+                    ivOne.setImageURI(image_uris.get(0));
+                    ivTwo.setImageURI(image_uris.get(1));
+                    ivThree.setImageURI(image_uris.get(2));
+                    ivFour.setImageURI(image_uris.get(3));
+                    ivFive.setImageURI(image_uris.get(4));
+                }
+
+        }
+    }
+
+
 }
